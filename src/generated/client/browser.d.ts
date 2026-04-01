8ef81fb4107042550843f470b143ff432f7563a4
Hammad Ali <ch.hammad2914@gmail.com>
2026-04-01 17:40:25 +0500

    new build

---
+++ src/generated/client/browser.d.ts
@@ -0,0 +1,30 @@
+import * as Prisma from './internal/prismaNamespaceBrowser';
+export { Prisma };
+export * as $Enums from './enums';
+export * from './enums';
+/**
+ * Model User
+ *
+ */
+export type User = Prisma.UserModel;
+/**
+ * Model OTP
+ *
+ */
+export type OTP = Prisma.OTPModel;
+/**
+ * Model Usage
+ *
+ */
+export type Usage = Prisma.UsageModel;
+/**
+ * Model Activity
+ *
+ */
+export type Activity = Prisma.ActivityModel;
+/**
+ * Model DailyUsage
+ *
+ */
+export type DailyUsage = Prisma.DailyUsageModel;
+//# sourceMappingURL=browser.d.ts.map
