7c28c76be03bc713502d3711bc680a128e6abdb7
Hammad Ali <ch.hammad2914@gmail.com>
2026-04-06 15:48:37 +0500

    Remove generated Prisma client files and related types from the project. This includes TypeScript definitions, JavaScript files, and associated source maps for the browser and client entry points, as well as common input types and enums.

--- src/generated/client/models.d.ts
+++
@@ -1,7 +0,0 @@
-export type * from './models/User';
-export type * from './models/OTP';
-export type * from './models/Usage';
-export type * from './models/Activity';
-export type * from './models/DailyUsage';
-export type * from './commonInputTypes';
-//# sourceMappingURL=models.d.ts.map
